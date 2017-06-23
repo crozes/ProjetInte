38d726949bb42cc5dd804bc42d64f2d896c8d99d
Cyril Crozes <cyril.crozes@gmail.com>
2017-06-23 13:54:17 +0200

    change

--- main.py
+++ main.py
@@ -219,14 +219,14 @@ def postNewPlayer() :
         
         db = Db()
         result = db.select(query_getName)
-        print result
-        dumpsResult = json.dumps(result)
-        print dumpsResult
+
+        #dumpsResult = json.dumps(result)
+        #print dumpsResult
         #resp = make_response(json.dumps(result)) 
         
-        for player in dumpsResult :
+        for player in data :
             print player
-            print "in for : "+player['player_name']
+            print "in for : "+ player['player_name']
             if player['player_name'].upper() == data['Player_name'].upper() :
                 data = {"IsAccepted" : False}
                 db.close()
