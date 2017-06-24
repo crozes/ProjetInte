d70ac6261aa8e287c6cd6608144c53c13c172f85
FabienB <fabien.beyly@gmail.com>
2017-06-24 23:01:13 +0200

    24/06
    Produit dans le recap
    recap modifié avec bouton pour valider et solde pour l'instant fictif

--- static/js/bootstrap-produit.js
+++ static/js/bootstrap-produit.js
@@ -1,23 +1,37 @@
+var prixProd = 5;
+
 function prodMoins(num){
-    var oldValue = $('#inputPub_' + num).val();
+    var oldValue = $('#inputProd_' + num).val();
     var valueParse = parseInt(oldValue);
     if(valueParse == 0){
-
+    }
+    else if(valueParse == 1){
+        $('#prod_' + num).remove();
+        var newValue = valueParse - 1;
+        $('#inputProd_' + num).val(newValue);
     }
     else{
+        var nom = $('#nomProd_' + num).text();
         var newValue = valueParse - 1;
-        $('#inputPub_' + num).val(newValue);
-        alert(newValue);
+        $('#inputProd_' + num).val(newValue);
+        $('#prod_' + num).empty();
+        $('#prod_' + num).append('<td>' + nom + '</td><td>' + (newValue * prixProd )+'€</td>');
     }
-});
+}
 
 function prodPlus(num){
-    var oldValue = $('#inputPub').val();
+    var oldValue = $('#inputProd_' + num).val();
     var valueParse = parseInt(oldValue);
     var newValue = valueParse + 1;
-    $('#inputPub').val(newValue);
-    pubChange();
-});
+    var nom = $('#nomProd_' + num).text();
+    if(valueParse == 0){
+        $('#recapAchat').append('<tr id="prod_' + num + '"><td>' + nom + '</td><td>' + prixProd +'€</td></tr>');
+    }
+    $('#prod_' + num).empty();
+    $('#prod_' + num).append('<td>' + nom + '</td><td>' + (newValue * prixProd )+'€</td>');
+    $('#inputProd_' + num).val(newValue);
+    
+}
 
 /*$.ajax('http://ponderosaproject.herokuapp.com/coucou').done(function(data){
     for(var i in data){
