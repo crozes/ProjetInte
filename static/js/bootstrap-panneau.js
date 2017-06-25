2e8b29d878132869d767608b3b255a29b6b70b1d
FabienB <fabien.beyly@gmail.com>
2017-06-25 14:42:48 +0200

    25/06 Récapitulatif fini

--- static/js/bootstrap-panneau.js
+++ static/js/bootstrap-panneau.js
@@ -5,31 +5,38 @@ function addPanneau(num){
 
     nombrePanneau += 1;
     var prixPub = 50;
-    $('#list').append('<tr id="line_' + nombrePanneau + '"><td>Panneau ' + nombrePanneau + '</td><td>x:<input id="x_' + nombrePanneau + '" value="10" class="input" type="text" name="lname"><BR>y:<input value="10" id="y_' + nombrePanneau + '" class="input" type="text" name="lname"></td><td><input id="surface_' + nombrePanneau + '" onkeypress="inputClick(' + nombrePanneau + ')" class="input" type="text" name="lname" value="1">m²</td><td id="tablePrixPub_' + nombrePanneau + '">50€</td><td><button type="button" id="clear_' + nombrePanneau + '" onclick="deletePanneau(' + nombrePanneau + ')" title="Clear checked items" class="btn btn-default col-sm-12">Supprimer</button></td></tr>');
-        
-    //
+    $('#list').append('<tr id="line_' + nombrePanneau + '"><td>Panneau ' + nombrePanneau + '</td><td>x:<input id="x_' + nombrePanneau + '" value="10" class="input" type="text" name="lname"><BR>y:<input value="10" id="y_' + nombrePanneau + '" class="input" type="text" name="lname"></td><td><input id="surface_' + nombrePanneau + '" onkeypress="inputClick(' + nombrePanneau + ')" class="input" type="text" name="lname" value="1">m²</td><td id="tablePrixPub_' + nombrePanneau + '">50€</td><td><button type="button" id="clear_' + nombrePanneau + '" onclick="deletePanneau(' + nombrePanneau + ')" class="btn btn-danger btn-circle btn-lg"><i class="glyphicon glyphicon-remove"></i></button></td></tr>');
+    modifSolde(nombrePanneau);
     addPubToRecap(nombrePanneau);
+    
 
 }
 
 function addPubToRecap(index){
     //On ajoute l'achat du panneau dans le recap
-    $('#recapAchat').append('<tr id="pub_' + index + '"><td>Pub</td><td>' + prix +'€</td></tr>');
+    $('#recapAchat').append('<tr id="pub_' + index + '"><td>Pub</td><td id="prixPub_' + index + '">' + prix +'€</td></tr>');
+    modifSolde(nombrePanneau);
 }
 
 function inputClick(num){
-    //Ici on refait un test pour récupérer la dernière valeur de l'input          
+    //Ici on refait un test pour récupérer la dernière valeur de l'input     
+    var old = $('#surface_' + num).val();
+    var oldValue = parseInt(old);
+
+
     $('#surface_' + num).on('input',function(){
         var data = $('#surface_' + num).val(); 
         var surface = parseInt(data);
+        var result = surface * prix;
 
         //On modifie le récap pour ce panneau
         $('#pub_' + num).empty();
-        $('#pub_' + num).append('<td>Pub</td><td>' + (surface * 50 )+'€</td>');
+        $('#pub_' + num).append('<td>Pub</td><td id="prixPub_' +num + '">' + result +'€</td>');
 
         //On modifie le prix dans le tableau de l'achat du panneau
         $('#tablePrixPub_' + num).empty();
-        $('#tablePrixPub_' + num).append('' + (surface * 50 )+'€');
+        $('#tablePrixPub_' + num).append('' + result +'€');
+        modifSolde(nombrePanneau);   
     });
 }
 
@@ -37,4 +44,5 @@ function deletePanneau(num){
     //On vide la ligne du panneau et le recap
     $('#line_' + num).empty();
     $('#pub_' + num).empty();
+    modifSolde(nombrePanneau);
 }
--- static/js/bootstrap-produit.js
+++ static/js/bootstrap-produit.js
@@ -9,13 +9,15 @@ function prodMoins(num){
         $('#prod_' + num).remove();
         var newValue = valueParse - 1;
         $('#inputProd_' + num).val(newValue);
+         modifSolde();
     }
     else{
         var nom = $('#nomProd_' + num).text();
         var newValue = valueParse - 1;
         $('#inputProd_' + num).val(newValue);
         $('#prod_' + num).empty();
-        $('#prod_' + num).append('<td>' + nom + '</td><td>' + (newValue * prixProd )+'€</td>');
+        $('#prod_' + num).append('<td>' + nom + '</td><td id="prixProd_' + num + '">' + (newValue * prixProd )+'€</td>');
+        modifSolde();
     }
 }
 
@@ -25,25 +27,60 @@ function prodPlus(num){
     var newValue = valueParse + 1;
     var nom = $('#nomProd_' + num).text();
     if(valueParse == 0){
-        $('#recapAchat').append('<tr id="prod_' + num + '"><td>' + nom + '</td><td>' + prixProd +'€</td></tr>');
+        $('#recapAchat').append('<tr id="prod_' + num + '"><td>' + nom + '</td><td id="prixProd_' + num + '">' + prixProd +'€</td></tr>');
     }
+
+    var prixglobal = newValue * prixProd;
     $('#prod_' + num).empty();
-    $('#prod_' + num).append('<td>' + nom + '</td><td>' + (newValue * prixProd )+'€</td>');
+    $('#prod_' + num).append('<td>' + nom + '</td><td id="prixProd_' + num + '">' + prixglobal +'€</td>');
     $('#inputProd_' + num).val(newValue);
+    modifSolde();
     
 }
 
-/*$.ajax('http://ponderosaproject.herokuapp.com/coucou').done(function(data){
-    for(var i in data){
-        $(document).on('click','#prodPlus_' + i,function(){
-            /*var oldValue = $('#inputProd_' + i).val();
-            var valueParse = parseInt(oldValue);
-            var newValue = valueParse + 1;
-            $('#inputProd_' + i).val(newValue);
-            prodChange(i);
-            
-        });
-        //alert(i);
+function modifSolde(nombrePanneau){
+    /*
+    //ancienne valeur du solde
+    $('#achatTotal').empty();
+    var soldeParse = parseInt(oldSolde);
+
+    //Valeur calculé
+    for(var i = 0; i < 100; i++){
+        $('#pub_' + i).val();
     }
-});*/
+    var newsolde = soldeParse + addVal - sousVal;
+    $('#achatTotal').text(newsolde);
+
+    var budget = $('#recapBudget').text();
+    $('#budgetTotal').text(budget);
+    var budgetParse = parseInt(budget);
+    var resultat = budgetParse - newsolde;
+    $('#resultat').text(resultat);*/
+    var total = 0;
+    for(var i = 0 ; i < 30 ; i++){
+        var valuePub = $('#prixPub_' + i).text();
+        var pubParse = parseInt(valuePub);
+        if(pubParse){
+            total += pubParse;
+        }
+        
+        var valueProd = $('#prixProd_' + i).text();
+        var prodParse = parseInt(valueProd);
+
+        if(prodParse){
+            total += prodParse;
+        }
+    }
+    //alert(total);
+
+    $('#achatTotal').text(total);
+
+    var budget = $('#recapBudget').text();
+    var budgetParse = parseInt(budget);
+    $('#budgetTotal').text(budgetParse);
+
+    var resultat = budgetParse - total;
+    $('#resultat').text(resultat);
+
+}
 
