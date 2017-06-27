8a3e0bcd9bceadeeee5d0d6aee144baba6dcf06f
Unknown <gaspard.desmoulins@gmail.com>
2017-06-27 09:25:56 +0200

    add new key playerinfo

--- main.py
+++ main.py
@@ -127,8 +127,7 @@ def getMap():
         for item in resultPlayerInfo:
             locationMapItem = {"latitude":item['mapitem_latitude'],"longitude":item['mapitem_longitude']}
             unMapItem={"kind":item['mapitem_kind'],"owner":player['player_name'],"location":locationMapItem,"influence":item['mapitem_rayon']}
-            unItem = {player['player_name']:unMapItem}
-            itemsByPlayers.append(unItem)
+            playersInfo[player['player_name']]=unMapItem}
         
         
         #-----------------------DRINKS_BY_PLAYER-----------------------
